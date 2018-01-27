41d8b50bc4bc2140f0f10b0b1e378cf2fb2e12da
Vlad <vlad.shaginyan@mail.ru>
2018-01-27 15:56:16 +0300

    works gulpfile with angular

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,56 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    connect = require('gulp-connect'),
-    plumber = require('gulp-plumber'),    
-    concat = require('gulp-concat');
+const gulp = require('gulp'),
+	  concat = require('gulp-concat'),
+	  less = require('gulp-less'),
+	  browserSync = require('browser-sync').create();
 
-gulp.task('connect', function() {
-    connect.server({
-        port: 8888,
-        root: './prod/',
-        livereload: true
-    });
+const scripts = require('./scripts');
+
+var devMode = false;
+
+gulp.task('less', function() {
+	gulp.src('./src/less/*.less')
+		.pipe(less())
+		.pipe(concat('styles.css'))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
-gulp.task('less', function(){
-   return gulp.src('./src/less/**/*.less')
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(gulp.dest('./prod/css'))
-        .pipe(connect.reload());
+gulp.task('js', function() {
+	gulp.src(scripts)
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
-gulp.task('scripts', function(){
-    return gulp.src('./src/js/**/*.js')
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('./prod/js'))
-        .pipe(connect.reload());
+gulp.task('html', function() {
+	return gulp.src('./src/templates/**/*.html')
+		.pipe(gulp.dest('./dist/'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
-gulp.task('html', function(){
-    return gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('./prod'))
-        .pipe(connect.reload());
+gulp.task('build', function() {
+	gulp.start(['less', 'js', 'html'])
 });
 
-gulp.task('watch', function(){
-    gulp.watch('./src/less/**/*.less', ['less'])
-    gulp.watch('./src/js/**/*.js', ['scripts'])
-    gulp.watch('./src/**/*.html', ['html'])    
+gulp.task('browser-sync', function() {
+	browserSync.init(null, {
+		open: false,
+		server: {
+			baseDir: 'dist',
+		}
+	});
 });
 
-gulp.task('default', ['connect', 'less', 'scripts', 'html', 'watch']);
+gulp.task('start', function() {
+	devMode = true;
+	gulp.start(['build', 'browser-sync']);
+	gulp.watch(['./src/less/**/*.less'], ['less']);
+	gulp.watch(['./src/js/**/*.js'], ['js']);
+	gulp.watch(['./src/templates/**/*.html'], ['html']);
+});
